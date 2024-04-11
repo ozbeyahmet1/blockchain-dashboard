770efe195f06485cc1fdb247bd450642f047347a
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-11 23:48:08 +0300

    feat: creates card component

---
+++ src/components/ui/logo/card/index.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import styles from "./card.module.scss";
+import { AnimationProps, motion } from "framer-motion";
+import Link from "next/link";
+import Image from "next/image";
+
+export interface CardProps {
+  index: number;
+  url: string;
+  name: string;
+  imageUrl: string;
+}
+
+export default function Card({ index, url, imageUrl, name }: CardProps) {
+  const cardMotionProps: AnimationProps = {
+    initial: { y: -100, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+    transition: { duration: index * 0.3 + 0.1, delay: 0.2 },
+  };
+
+  return (
+    <Link href={url} className={styles["card"]}>
+      <motion.div className={styles["card_single"]} {...cardMotionProps}>
+        <Image className={styles["card_image"]} src={imageUrl} width={80} height={80} alt="" />
+        <p className={styles["card_title"]}>{name}</p>
+      </motion.div>
+    </Link>
+  );
+}
