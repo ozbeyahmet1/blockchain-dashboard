0563ac0427415a3da99554c9b52e2b6d5ae029b5
Ahmet Ozbey <ahmet1ozbey@gmail.com>
2024-04-11 23:49:26 +0300

    feat: creates sample blockchain data and adds to view

--- src/views/homepage/index.tsx
+++ src/views/homepage/index.tsx
@@ -2,6 +2,8 @@ import { AnimationProps, motion } from "framer-motion";
 import { BsLightningFill } from "react-icons/bs";
 import { HomepageProps } from "@/helpers/interfaces/pageProps";
 import styles from "./homepage.module.scss";
+import Card from "@/components/ui/logo/card";
+import { blockchainDatas } from "@/datas/blockchainData";
 export default function HomepageView({ ui: { description, header, subHeader } }: HomepageProps) {
   const headerMotionProps: AnimationProps = {
     initial: { y: -100 },
@@ -19,10 +21,19 @@ export default function HomepageView({ ui: { description, header, subHeader } }:
         </h3>
         <h5 className={styles["homepage_description"]}>{description}</h5>
         <div className={styles["grid-container"]}>
-
+          {blockchainDatas.map((blockchainData, i) => {
+            return (
+              <Card
+                key={i}
+                index={i}
+                imageUrl={blockchainData.logo}
+                name={blockchainData.name}
+                url={"/" + blockchainData.name}
+              />
+            );
+          })}
         </div>
       </div>
-
     </div>
   );
 }
